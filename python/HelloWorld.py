d08a803b9fea12d6ecaa59d7a9b42ac81557633e
alexdecei <alexdeceita@gmail.com>
2019-02-25 14:57:14 +0100

    dfds

--- python/HelloWorld.py
+++ python/HelloWorld.py
@@ -1,5 +1,6 @@
 import asyncio
 from xknx import XKNX
+from xknx.devices import Light
 """
 LAMPES
 0/1/1...4
@@ -11,10 +12,9 @@ BOUTONS
 0/3/1...4
 
 SSID: FING
-MDP: maquettefing
+MDP: maquettefing1
 
 """
-
 
 #fonction callback lors de la reception d'un telegram
 async def telegram_received_cb(telegram):
@@ -30,26 +30,21 @@ async def main():
             own_address= Address,
             telegram_received_cb=print("coucou"),
             device_updated_cb=print("truc"))
-    
+
     #ajout d'un device
     light = Light(xknx,
                   name='TestLight',
                   group_address_switch='0/1/1',
                   group_address_brightness='1/2/5')
-    xknx.devices.add(light)
 
     #créé l'objet knx, peut prendre des args
     await xknx.start(state_updater=False, daemon_mode=True)
     #state: lance le processus asynchrone, synchronise les devices toutes les heure
     
     # faire des trucs avec la light
-    await xknx.devices['TestLight'].set_on()
-    await xknx.devices['TestLight'].set_brightness(23)
-    await xknx.devices['TestLight'].set_off()
-
-    
-
-
+    await light.set_on()
+    await asyncio.sleep(2)
+    await light.set_off()
     await xknx.stop()
     #déconnexion des tunnels
 
