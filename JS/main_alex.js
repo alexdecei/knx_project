07e588bd6507df8c89a33c43686f3c504bfe6760
alexdecei <alexdeceita@gmail.com>
2019-03-18 14:05:15 +0100

    ajout function synch

--- JS/main_alex.js
+++ JS/main_alex.js
@@ -1,14 +1,72 @@
-console.log("go!")
+
+
 var knx = require('knx');
-var connection = knx.Connection({
- handlers: {
-  connected: function() {
-    console.log('Connected!');
-  },
-  event: function (evt, src, dest, value) {
-  console.log("%s **** KNX EVENT: %j, src: %j, dest: %j, value: %j",
-    new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-    evt, src, dest, value);
+
+console.log("coucou1")
+var connection = new knx.Connection( {
+  // ip address and port of the KNX router or interface
+  ipAddr: '192.168.0.5', ipPort: 3671,
+  // the KNX physical address we'd like to use
+  //physAddr: '15.15.249',
+  // set the log level for messsages printed on the console. This can be 'error', 'warn', 'info' (default), 'debug', or 'trace'.
+  loglevel: 'info',
+  // do not automatically connect, but use connection.Connect() to establish connection
+  manualConnect: false,
+  // use tunneling with multicast (router) - this is NOT supported by all routers! See README-resilience.md
+  forceTunneling: true,
+  // wait at least 10 millisec between each datagram
+  minimumDelay: 10,
+  // enable this option to suppress the acknowledge flag with outgoing L_Data.req requests. LoxOne needs this
+  suppress_ack_ldatareq: false,
+  // define your event handlers here:
+  handlers: {
+    // wait for connection establishment before sending anything!
+    connected: function() {
+      console.log('Hurray, I can talk KNX!');
+      chenillard(4, 250)
+
+    },
+    // get notified for all KNX events:
+    event: function(evt, src, dest, value) { console.log(
+        "event: %s, src: %j, dest: %j, value: %j",
+        evt, src, dest, value
+      );
+    },
+    // get notified on connection errors
+    error: function(connstatus) {
+      console.log("**** ERROR: %j", connstatus);
+    }
   }
- }
 });
+
+async function chenillard(nbre, tps){
+  // Initialisation à 0
+  connection.write("0/1/1", 0);
+  connection.write("0/1/2", 0);
+  connection.write("0/1/3", 0);
+  connection.write("0/1/4", 0);
+  sleepSYNC(250)
+  //Chenillards
+  for (i = 0; i < nbre; i++) {
+    connection.write("0/1/1", 1);
+    sleepSYNC(tps)
+    connection.write("0/1/1", 0);
+    connection.write("0/1/2", 1);
+    sleepSYNC(tps)
+    connection.write("0/1/2", 0);
+    connection.write("0/1/3", 1);
+    sleepSYNC(tps)
+    connection.write("0/1/3", 0);
+    connection.write("0/1/4", 1);
+    sleepSYNC(tps)
+    connection.write("0/1/4", 0);
+  }
+}
+
+function sleepSYNC(temps){
+  return(promise1 = new Promise(function(resolve, reject) {
+    setTimeout(temps);
+  }));
+}
+
+console.log("coucou2")
